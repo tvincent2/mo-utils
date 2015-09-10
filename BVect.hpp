d253719dd6dd2a0832a9f1bf24ac875c13ce2bce
Thomas Vincent <thomas@vinc-net.fr>
2015-09-10 22:20:30 +0200

    Add comparison operators and methods

--- BVect.hpp
+++ BVect.hpp
@@ -14,6 +14,21 @@ class BVect {
     BVect (double y1, double y2, vector<double> x) : _y1(y1), _y2(y2), _x(x) {}
     double y1() const {return _y1;}
     double y2() const {return _y2;}
+    bool operator == (const BVect& bv) const {
+      return (this->y1() == bv.y1() && this->y2() == bv.y2());
+    }
+    bool operator != (const BVect bv) const {
+      return !(*this == bv);
+    }
+    bool weaklyDominates(const BVect& bv) const {
+      return (this->y1() <= bv.y1() && this->y2() <= bv.y2());
+    }
+    bool dominates(const BVect& bv) const {
+      return (this->weaklyDominates(bv) && (*this != bv));
+    }
+    bool strictlyDominates(const BVect& bv) const {
+      return (this->y1() < bv.y1() && this->y2() < bv.y2());
+    }
 };
 
 #endif
