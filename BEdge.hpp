30897f052edd4033c7fa0b6c164fe02c9f747bd2
Thomas Vincent <thomas@vinc-net.fr>
2015-09-11 15:20:11 +0200

    BEdge: Make attributes private and add getters

--- BEdge.hpp
+++ BEdge.hpp
@@ -4,14 +4,18 @@
 #include "BVect.hpp"
 
 class BEdge {
-  public:
-    BEdge (BVect a, BVect b, bool aC, bool bC){_a = a; _b = b; _aClosed = aC; _bClosed = bC;}
-    BEdge (BVect a, BVect b){_a = a; _b = b; _aClosed = true; _bClosed = true;}
-    BEdge (BVect a){_a = a; _b = a; aClosed = true; bClosed = true;}
+  private:
     BVect _a;
     BVect _b;
     bool _aClosed;
     bool _bClosed;
+    bool _point;
+  public:
+    BEdge (BVect a, BVect b, bool aC = true, bool bC = true) : _a(a), _b(b), _aClosed(aC), _bClosed(bC), _point(a==b) {}
+    BEdge (BVect a) : _a(a), _b(a), _aClosed(true), _bClosed(true), _point(true) {}
+    BVect leftPoint() const {return _a;}
+    BVect rightPoint() const {return _b;}
+    bool isAPoint() const {return _point;}
 };
 
 #endif
