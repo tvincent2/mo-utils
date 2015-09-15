4edfed98e444e6c6dac076ac76f3efdb4e9ab5ea
Thomas Vincent <thomas@vinc-net.fr>
2015-09-15 11:52:41 +0200

    Allow to invert DominanceStatus

--- DominanceStatus.hpp
+++ DominanceStatus.hpp
@@ -10,4 +10,14 @@ enum class DominanceStatus {
   MUTUAL_DOM
 };
 
+inline DominanceStatus invert(DominanceStatus ds) {
+  switch (ds) {
+    case DominanceStatus::A_DOM_B : return DominanceStatus::B_DOM_A;
+    case DominanceStatus::B_DOM_A : return DominanceStatus::A_DOM_B;
+    case DominanceStatus::A_PART_DOM_B : return DominanceStatus::B_PART_DOM_A;
+    case DominanceStatus::B_PART_DOM_A : return DominanceStatus::A_PART_DOM_B;
+    default : return ds;
+  };
+}
+
 #endif  // DOMINANCE_STATUS_HPP
