b3095f20e64b76110dbf6a8aac6847fce717b5b2
Thomas Vincent <thomas@vinc-net.fr>
2015-09-16 21:19:53 +0200

    BVect: Fix coordinates getters names to z1 and z2

--- BVect.hpp
+++ BVect.hpp
@@ -7,35 +7,35 @@ using namespace std;
 
 class BVect {
   private:
-    double _y1;
-    double _y2;
+    double _z1;
+    double _z2;
     vector<double> _x;
   public:
-    BVect (double y1, double y2, vector<double> x) : _y1(y1), _y2(y2), _x(x) {}
+    BVect (double z1, double z2, vector<double> x) : _z1(z1), _z2(z2), _x(x) {}
     BVect (const BVect& b1, const BVect& b2, double lambda) {
-      _y1 = lambda * b1.y1() + (1-lambda) * b2.y1();
-      _y2 = lambda * b1.y2() + (1-lambda) * b2.y2();
+      _z1 = lambda * b1.z1() + (1-lambda) * b2.z1();
+      _z2 = lambda * b1.z2() + (1-lambda) * b2.z2();
       for (unsigned i = 0; i < b1.x().size(); ++i) {
         _x.push_back(lambda * b1.x().at(i) + (1-lambda) * b2.x().at(i));
       }
     }
-    double y1() const {return _y1;}
-    double y2() const {return _y2;}
+    double z1() const {return _z1;}
+    double z2() const {return _z2;}
     vector<double> x() const {return _x;}
     bool operator == (const BVect& bv) const {
-      return (this->y1() == bv.y1() && this->y2() == bv.y2());
+      return (this->z1() == bv.z1() && this->z2() == bv.z2());
     }
     bool operator != (const BVect bv) const {
       return !(*this == bv);
     }
     bool weaklyDominates(const BVect& bv) const {
-      return (this->y1() <= bv.y1() && this->y2() <= bv.y2());
+      return (this->z1() <= bv.z1() && this->z2() <= bv.z2());
     }
     bool dominates(const BVect& bv) const {
       return (this->weaklyDominates(bv) && (*this != bv));
     }
     bool strictlyDominates(const BVect& bv) const {
-      return (this->y1() < bv.y1() && this->y2() < bv.y2());
+      return (this->z1() < bv.z1() && this->z2() < bv.z2());
     }
     bool operator < (const BVect& bv) const {
       return this->strictlyDominates(bv);
@@ -44,7 +44,7 @@ class BVect {
       return this->dominates(bv);
     }
     bool isInA1AreaOf(const BVect& bv) const {
-      return (bv.y1() > this->y1() && bv.y2() < this->y2());
+      return (bv.z1() > this->z1() && bv.z2() < this->z2());
     }
     bool isInA2AreaOf(const BVect& bv) const {
       return bv.isInA1AreaOf(*this);
