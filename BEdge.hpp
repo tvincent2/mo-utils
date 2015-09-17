329b3501b03d8e2e8a69e4fecc7feaa290c0e6f9
Thomas Vincent <thomas@vinc-net.fr>
2015-09-17 23:39:30 +0200

    BEdge: Add comparison between edges

--- BEdge.hpp
+++ BEdge.hpp
@@ -107,6 +107,101 @@ class BEdge {
         return invert(compareRightEdges(b, t));
       }
     }
+    DominanceStatus compareWith(const BEdge& be, BEdge& tleft, BEdge& tright, BEdge& beleft, BEdge& beright) const {
+      if (this->isAPoint() && be.isAPoint()) {
+        if (this->leftPoint().dominates(be.leftPoint()))
+          return DominanceStatus::A_DOM_B;
+        else if (be.leftPoint().dominates(this->leftPoint()))
+          return DominanceStatus::B_DOM_A;
+        else return DominanceStatus::NO_DOM;
+      } else if (this->isAPoint()) {
+        return invert(be.compareWithPoint(*this, tleft, tright));
+      } else if (be.isAPoint()) {
+        return this->compareWithPoint(be, beleft, beright);
+      } else {
+        double lambda;
+        if (this->intersect(be, lambda)) {
+          BVect intersection(this->leftPoint(), this->rightPoint(), lambda);
+          tleft = BEdge(this->leftPoint(), intersection);
+          tright = BEdge(intersection, this->rightPoint());
+          beleft = BEdge(be.leftPoint(), intersection);
+          beright = BEdge(intersection, be.rightPoint());
+          DominanceStatus left = compareLeftEdges(tleft, beleft);
+          DominanceStatus right = compareRightEdges(tright, beright);
+          return DominanceStatus::MUTUAL_DOM;
+        } else {
+          if (this->isInA1AreaOf(be) || this->isInA2AreaOf(be))
+            return DominanceStatus::NO_DOM;
+          std::pair<double, double> lambdasb1 = this->computeLambdasWithPoint(be.leftPoint());
+          BVect proj1bleft(this->leftPoint(), this->rightPoint(), lambdasb1.first);
+          BVect proj2bleft(this->leftPoint(), this->rightPoint(), lambdasb1.second);
+          std::pair<double, double> lambdasb2 = this->computeLambdasWithPoint(be.rightPoint());
+          BVect proj1bright(this->leftPoint(), this->rightPoint(), lambdasb2.first);
+          BVect proj2bright(this->leftPoint(), this->rightPoint(), lambdasb2.second);
+          std::pair<double, double> lambdast1 = this->computeLambdasWithPoint(this->leftPoint());
+          BVect proj1tleft(be.leftPoint(), be.rightPoint(), lambdast1.first);
+          BVect proj2tleft(be.leftPoint(), be.rightPoint(), lambdast1.second);
+          std::pair<double, double> lambdast2 = this->computeLambdasWithPoint(this->rightPoint());
+          BVect proj1tright(be.leftPoint(), be.rightPoint(), lambdast2.first);
+          BVect proj2tright(be.leftPoint(), be.rightPoint(), lambdast2.second);
+          bool adomb = false;
+          bool bdoma = false;
+          bool apartdomb = false;
+          bool bpartdoma = false;
+          if (be.leftPoint().weaklyDominates(proj1bleft)) {
+            bdoma = true;
+            if (proj1bleft != this->leftPoint()) {
+              tleft = BEdge(this->leftPoint(), proj1bleft);
+              bpartdoma = true;
+            }
+          }
+          if (be.rightPoint().weaklyDominates(proj2bright)) {
+            bdoma = true;
+            if (proj2bright != this->rightPoint()) {
+              tright = BEdge(proj2bright, this->rightPoint());
+              bpartdoma = true;
+            }
+          }
+          if (proj2bleft.weaklyDominates(be.leftPoint())) {
+            adomb = true;
+          }
+          if (proj1bright.weaklyDominates(be.rightPoint())) {
+            adomb = true;
+          }
+          if (this->leftPoint().weaklyDominates(proj1tleft)) {
+            adomb = true;
+            if (proj1tleft != be.leftPoint()) {
+              beleft = BEdge(be.leftPoint(), proj1tleft);
+              apartdomb = true;
+            }
+          }
+          if (this->rightPoint().weaklyDominates(proj2tright)) {
+            adomb = true;
+            if (proj2tright != be.rightPoint()) {
+              beright = BEdge(proj2tright, be.rightPoint());
+              apartdomb = true;
+            }
+          }
+          if (proj2tleft.weaklyDominates(this->leftPoint())) {
+            bdoma = true;
+          }
+          if (proj1tright.weaklyDominates(this->rightPoint())) {
+            bdoma = true;
+          }
+          if (adomb && bdoma)
+            return DominanceStatus::MUTUAL_DOM;
+          if (apartdomb)
+            return DominanceStatus::A_PART_DOM_B;
+          if (adomb)
+            return DominanceStatus::A_DOM_B;
+          if (bpartdoma)
+            return DominanceStatus::B_PART_DOM_A;
+          if (bdoma)
+            return DominanceStatus::B_DOM_A;
+          return DominanceStatus::NO_DOM;
+        }
+      }
+    }
 };
 
 #endif
