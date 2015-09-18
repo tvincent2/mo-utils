74db96c48048fb6d61a3e30dca8d6fe833e1c935
Thomas Vincent <thomas@vinc-net.fr>
2015-09-18 23:44:02 +0200

    BEdge: Add test for projection computation

--- test/BEdgeTest.cpp
+++ test/BEdgeTest.cpp
@@ -6,12 +6,14 @@ class BEdgeTest : public CppUnit::TestFixture {
   CPPUNIT_TEST(testPointGetters);
   CPPUNIT_TEST(testIsAPoint);
   CPPUNIT_TEST(testAreaPredicates);
+  CPPUNIT_TEST(testProjComputation);
   CPPUNIT_TEST_SUITE_END();
  private:
   BEdge* simpleEdge;
   BEdge* pointEdge;
   BEdge* pointEdge2;
   BEdge* simpleEdge2;
+  BEdge* longEdge;
  public:
   void setUp() {
     std::vector<double> x;
@@ -24,12 +26,14 @@ class BEdgeTest : public CppUnit::TestFixture {
     pointEdge = new BEdge(c);
     pointEdge2 = new BEdge(c, c);
     simpleEdge2 = new BEdge(d, e);
+    longEdge = new BEdge(a, e);
   }
   void tearDown() {
     delete simpleEdge;
     delete pointEdge;
     delete pointEdge2;
     delete simpleEdge2;
+    delete longEdge;
   }
   void testPointGetters() {
     CPPUNIT_ASSERT(simpleEdge->leftPoint().z1() == 1);
@@ -63,6 +67,25 @@ class BEdgeTest : public CppUnit::TestFixture {
     CPPUNIT_ASSERT(!pointEdge->isInA2AreaOf(*simpleEdge));
     CPPUNIT_ASSERT(!pointEdge->isInA1AreaOf(*simpleEdge2));
     CPPUNIT_ASSERT(!pointEdge->isInA2AreaOf(*simpleEdge2));
+  }
+  void testProjComputation() {
+    std::vector<double> x;
+    BVect zero(0, 0, x);
+    BVect a(0, 9, x);
+    BVect b(3, 5, x);
+    BVect c(2, 8, x);
+    std::pair<double, double> pz = longEdge->computeLambdasWithPoint(zero);
+    std::pair<double, double> pa = longEdge->computeLambdasWithPoint(a);
+    std::pair<double, double> pb = longEdge->computeLambdasWithPoint(b);
+    std::pair<double, double> pc = longEdge->computeLambdasWithPoint(c);
+    CPPUNIT_ASSERT(pz.first == 0);
+    CPPUNIT_ASSERT(pz.second == 1);
+    CPPUNIT_ASSERT(pa.first == 0);
+    CPPUNIT_ASSERT(pa.second == 0.25);
+    CPPUNIT_ASSERT(pb.first == 2.0/3.0);
+    CPPUNIT_ASSERT(pb.second == 1);
+    CPPUNIT_ASSERT(pc.first == 1.0/3.0);
+    CPPUNIT_ASSERT(pc.second == 0.5);
   }
 };
 
