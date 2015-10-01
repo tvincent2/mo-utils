2a55c0ae8803d066f718c99f281571545bf39b72
Thomas Vincent <thomas@vinc-net.fr>
2015-10-01 23:04:20 +0200

    DominanceStatus: Add test class

---
+++ test/DominanceStatusTest.cpp
@@ -0,0 +1,36 @@
+#include <cppunit/extensions/HelperMacros.h>
+#include "DominanceStatus.hpp"
+
+
+class DominanceStatusTest : public CppUnit::TestFixture {
+  CPPUNIT_TEST_SUITE(DominanceStatusTest);
+  CPPUNIT_TEST(testDominanceStatus);
+  CPPUNIT_TEST_SUITE_END();
+ private:
+  DominanceStatus nd;
+  DominanceStatus adb;
+  DominanceStatus apdb;
+  DominanceStatus bda;
+  DominanceStatus bpda;
+  DominanceStatus md;
+ public:
+  void setUp() {
+    nd = DominanceStatus::NO_DOM;
+    adb = DominanceStatus::A_DOM_B;
+    apdb = DominanceStatus::A_PART_DOM_B;
+    bda = DominanceStatus::B_DOM_A;
+    bpda = DominanceStatus::B_PART_DOM_A;
+    md = DominanceStatus::MUTUAL_DOM;
+  }
+  void tearDown() {}
+  void testDominanceStatus() {
+    CPPUNIT_ASSERT (nd == DominanceStatus::NO_DOM);
+    CPPUNIT_ASSERT (adb == DominanceStatus::A_DOM_B);
+    CPPUNIT_ASSERT (apdb == DominanceStatus::A_PART_DOM_B);
+    CPPUNIT_ASSERT (bda == DominanceStatus::B_DOM_A);
+    CPPUNIT_ASSERT (bpda == DominanceStatus::B_PART_DOM_A);
+    CPPUNIT_ASSERT (md == DominanceStatus::MUTUAL_DOM);
+  }
+};
+
+CPPUNIT_TEST_SUITE_REGISTRATION(DominanceStatusTest);
